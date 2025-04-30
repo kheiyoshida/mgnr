ca2644ade716d06bd2908c551991aeb6f8522b6a
Katsumi Yoshida <kheiyoshida@gmail.com>
2025-04-30 16:05:13 +1000

    feat(website): embed cli usage example

--- web/website/src/app/cli/page.tsx
+++ web/website/src/app/cli/page.tsx
@@ -19,6 +19,14 @@ export default function CliPage() {
 
       <Section title={'Installation'} />
       <code className="my-2">npm i @mgnr/cli</code>
+
+      <Section title={'Example'} />
+      <iframe
+        width="100%"
+        height={'835px'}
+        allow="clipboard-write"
+        src="https://emgithub.com/iframe.html?target=https%3A%2F%2Fgithub.com%2Fkheiyoshida%2Fmgnr%2Fblob%2Fmain%2Fpkgs%2Fmgnr-cli%2Fsrc%2Fsample.ts&style=default&type=code&showBorder=on&showLineNumbers=on&showFileMeta=on&showFullPath=on&showCopy=on"
+      ></iframe>
     </PageBody>
   )
 }
